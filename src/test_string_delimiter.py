05bc041f89d0bb7ead1b2417ef84bba5f6e4eaf0
brandonscottred <brandon.redford@outlook.com>
2025-05-05 14:09:18 -0600

    add string_delimiter tests to text_...

--- src/test_string_delimiter.py
+++ src/test_string_delimiter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from string_delimiter import split_nodes_delimiter
+from string_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_split_with_code_delimiter(self):
@@ -27,6 +27,42 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         ]
 
         self.assertEqual(actual_nodes, expected_nodes)
+
+class TestImagesLinksExtraction(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    
+    def test_split_links(self):
+        node = TextNode(
+            "Check out this [great resource](https://example.com) and also visit [another site](https://test.org) for more info.",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            [
+                TextNode("Check out this ", TextType.TEXT),
+                TextNode("great resource", TextType.LINK, "https://example.com"),
+                TextNode(" and also visit ", TextType.TEXT),
+                TextNode("another site", TextType.LINK, "https://test.org"),
+                TextNode(" for more info.", TextType.TEXT),                
+            ],
+            new_nodes
+        )
 
 if __name__ == "__main__":
     unittest.main()
